abb081bb98acdb9254792440ca000a39464d1b94
Nehal Ali <nehalali7867@gmail.com>
2025-08-06 16:53:00 +0500

    Learnt basics of the netmiko library

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+
+huawei_gpon_olt = {
+    'device_type' : 'huawei_olt_telnet',
+    'ip' : '192.168.209.209',
+    'username' : 'optomenoc',
+    'password' : 'letmein@123',
+    'port' : 23
+}
+
+net_connect = ConnectHandler(**huawei_gpon_olt)
+net_connect.enable()
+output = net_connect.send_command_expect('display ont autofind all')
+print(output)
